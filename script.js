99c213fdd7825289203c109a02618afa851f37a6
graholm <graholm@nuevaschool.org>
2016-12-02 08:49:43 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var editor = $(parent.document.body)
 editor.find("#project-files-header").css({color: "green"})
 
-editor.find("div").css({border:"1px solid blue"})
+//editor.find("div").css({border:".5px solid blue"});
+//editor.find("").css({border:".5px solid blue"});
+
+
 var topbar = $(parent.parent.document.body).find("#topbar")
 
 
