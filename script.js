0ac11dce804678f7d19bf53789d5f0bca8c79e1c
graholm <graholm@nuevaschool.org>
2016-12-02 09:00:48 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var Button = $("#btn btn-primary")
-var editor = $(parent.document.body)
-editor.find("#project-files-header").css({color: "green"})
+
 
 
  
-var topbar = $(parent.parent.document.body).find("#topbar")
+
 
 Button.click(function() {
+  
   console.log ("go")
+  var editor = $(parent.document.body)
+  editor.find("#project-files-header").css({color: "pink"})
+  var topbar = $(parent.parent.document.body).find("#topbar")
   topbar.css({background: "pink"})
   editor.find("div").css({border:"1px solid pink"});
   
