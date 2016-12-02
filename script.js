3caa6d8fe68a8076ba14f6696213195ab7e2a060
graholm <graholm@nuevaschool.org>
2016-12-02 08:59:40 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var Button = $("#btn btn-primary")
 var editor = $(parent.document.body)
 editor.find("#project-files-header").css({color: "green"})
 
-//editor.find("div").css({border:"1px solid pink"});
+
  
 var topbar = $(parent.parent.document.body).find("#topbar")
 
@@ -14,7 +14,7 @@ Button.click(function() {
 })
 
 
-
+//editor.find("div").css({border:"1px solid pink"});
 
 // Now try to
 // 1. add a button to the topbar
