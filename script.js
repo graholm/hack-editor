3923066254cc865c98d778be79a530467e252f91
graholm <graholm@nuevaschool.org>
2016-12-14 09:26:43 -0800

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,19 @@
+var buutton = $("#coolbutton")
+
+
+buutton.click(function() {
+  
+  console.log ("go")
+  var editor = $(parent.document.body)
+  editor.find("#project-files-header").css({color: "pink"})
+  var topbar = $(parent.parent.document.body).find("#topbar")
+  topbar.css({background: "pink"})
+  editor.find("div").css({border:"1px solid pink"});
+  
+})
+
+// Now try to
+// 1. add a button to the topbar
+// 2. change the title above the file list
+// 3. change some borders in the editor
+// 4. Make all this happen when a button is clicked
